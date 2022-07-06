fc9a8c14a11ddfef155b9698d4b6bfd23beb5096
xbdirisxk <abdirisakjr@gmail.com>
2022-07-07 00:23:20 +0300

    add login & signup components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-// import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
+import LogIn from "./components/Login";
 
 function App() {
   return (
     <div>
-      <SignUp />
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
     </div>
   );
 }
